d8960bb568cfa308924af9ce42fd016f94f7cbfd
Kelley Reynolds <kelley@water5000.com>
2026-03-02 18:02:17 -0500

    Remove redundant param table init calls and clean up extconf
    init_cctx_param_table() and init_dctx_param_table() were called twice at
    startup: once inside their respective init_class() functions and again
    redundantly in Init_vibe_zstd(). Remove the duplicate calls from
    Init_vibe_zstd() since the init_class functions already handle it.
    
    Also remove the unused deprecated/ zstd source directory from extconf.rb
    and apply minor style consistency fixes (single-quoted strings).

--- ext/vibe_zstd/extconf.rb
+++ ext/vibe_zstd/extconf.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require "mkmf"
+require 'mkmf'
 
 # Use vendored zstd library
-LIBZSTD_DIR = File.expand_path("libzstd", __dir__)
+LIBZSTD_DIR = File.expand_path('libzstd', __dir__)
 
 # Add include paths for vendored zstd headers
 # standard:disable Style/GlobalVars
@@ -15,38 +15,36 @@ $INCFLAGS << " -I#{LIBZSTD_DIR}/dictBuilder"
 # standard:enable Style/GlobalVars
 
 # Add preprocessor definitions
-append_cflags("-DXXH_NAMESPACE=ZSTD_")
-append_cflags("-DZSTD_LEGACY_SUPPORT=0")  # Disable legacy support to reduce size
-append_cflags("-DZSTD_MULTITHREAD")  # Enable multithreading support
+append_cflags('-DXXH_NAMESPACE=ZSTD_')
+append_cflags('-DZSTD_LEGACY_SUPPORT=0') # Disable legacy support to reduce size
+append_cflags('-DZSTD_MULTITHREAD') # Enable multithreading support
 
 # Link with pthread for multithreading
-have_library("pthread") || abort("pthread library is required for multithreading support")
+have_library('pthread') || abort('pthread library is required for multithreading support')
 
 # Makes all symbols private by default to avoid unintended conflict
 # with other gems. To explicitly export symbols you can use RUBY_FUNC_EXPORTED
 # selectively, or entirely remove this flag.
-append_cflags("-fvisibility=hidden")
+append_cflags('-fvisibility=hidden')
 
 # Gather all vendored zstd source files (including assembly with arch guards)
 zstd_sources = Dir[
   "#{LIBZSTD_DIR}/common/*.c",
   "#{LIBZSTD_DIR}/compress/*.c",
   "#{LIBZSTD_DIR}/decompress/*.{c,S}",
-  "#{LIBZSTD_DIR}/dictBuilder/*.c",
-  "#{LIBZSTD_DIR}/deprecated/*.c"
+  "#{LIBZSTD_DIR}/dictBuilder/*.c"
 ].map { |path| File.basename(path) }
 
 # Add the main vibe_zstd.c file (which includes the split files via #include)
 # standard:disable Style/GlobalVars
-$srcs = ["vibe_zstd.c"] + zstd_sources
+$srcs = ['vibe_zstd.c'] + zstd_sources
 
 # Set vpath to find source files in subdirectories
 $VPATH ||= []
-$VPATH << "$(srcdir)/libzstd/common"
-$VPATH << "$(srcdir)/libzstd/compress"
-$VPATH << "$(srcdir)/libzstd/decompress"
-$VPATH << "$(srcdir)/libzstd/dictBuilder"
-$VPATH << "$(srcdir)/libzstd/deprecated"
+$VPATH << '$(srcdir)/libzstd/common'
+$VPATH << '$(srcdir)/libzstd/compress'
+$VPATH << '$(srcdir)/libzstd/decompress'
+$VPATH << '$(srcdir)/libzstd/dictBuilder'
 # standard:enable Style/GlobalVars
 
-create_makefile("vibe_zstd/vibe_zstd")
+create_makefile('vibe_zstd/vibe_zstd')
